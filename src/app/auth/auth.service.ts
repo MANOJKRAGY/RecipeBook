aca0cf7013630ab9fe7e9dce906c49c7e82fc12d
MANOJKRAGY <38702501+MANOJKRAGY@users.noreply.github.com>
2020-08-04 12:47:11 +0530

    Update and put in prod

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { throwError, Subject,BehaviorSubject } from 'rxjs';
 import { catchError, tap } from "rxjs/operators";
 import { User } from "./user.model";
 import {Router} from '@angular/router';
+import {environment} from '../../environments/environment';
 
 export interface AuthResponseData{
     idToken:string;
@@ -23,8 +24,8 @@ export class AuthService{
     constructor(private http: HttpClient,private router:Router){}
     signup(email:string ,password:string){
 
-     return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA9HwCPLUV-hfFZqWqjgHdSTTSuetn4PbA'
-     ,{
+     return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='+environment.firebaseAPIKey ,
+     {
          email:email,
          password:password,
          returnSecureToken:true
@@ -40,7 +41,7 @@ export class AuthService{
     }
 
     login(email:string,password:string){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA9HwCPLUV-hfFZqWqjgHdSTTSuetn4PbA',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='+environment.firebaseAPIKey,
         {
             email:email,
             password:password,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey :'AIzaSyA9HwCPLUV-hfFZqWqjgHdSTTSuetn4PbA'
 };
